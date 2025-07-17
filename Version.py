fbbd19859adb200a3139cdd8dd58b2c450584ef3
gttrcr <gttrcr@gmail.com>
2025-07-17 13:15:01 +0200

    version and check for updates

--- Version.py
+++ Version.py
@@ -1,3 +1,10 @@
+import os
+import sys
+import json
+import gdown
+import tempfile
+
+
 class Version:
     def __init__(self, major=None, minor=None, patch=None):
         self.major = major
@@ -37,3 +44,33 @@ class Version:
             return 1
 
         return 0
+
+    def control(self, gdown_id, app_id):
+        new_file, output = tempfile.mkstemp()
+        url = f"https://drive.google.com/uc?id={gdown_id}"
+        gdown.download(url=url, output=output, quiet=True)
+
+        d = []
+        with open(output) as f:
+            d = json.load(f)
+
+        os.remove(output)
+
+        obj = next(
+            (item for item in d if item.get("n") == app_id),
+            None,
+        )
+
+        if obj:
+            v = Version().from_string(obj["v"])
+            if self.timeline(v) == 1:
+                self.__updatable = obj
+                return True
+
+        return False
+
+    def update(self):
+        new_file, output = tempfile.mkstemp()
+        url = f"https://drive.google.com/uc?id={self.__updatable["u"]}"
+        gdown.download(url=url, output=output, quiet=True)
+        # os.execl(sys.executable, *sys.orig_argv)
