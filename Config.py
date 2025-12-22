c3b9defb09aaab06db970e280cfa699d54f648ab
gttrcr <gttrcr@gmail.com>
2025-12-22 13:54:34 +0100

    optional default configuration

--- Config.py
+++ Config.py
@@ -1,26 +1,27 @@
+import os
 import json
 
 
 class Config:
     _instance = None
 
-    def __new__(cls, conf=None):
+    def __new__(cls, conf=None, default=None):
         if cls._instance is None:
-            if conf:
-                cls._instance = super(Config, cls).__new__(cls)
-            else:
-                raise Exception("Cannot instance without a configuration file")
+            cls._instance = super(Config, cls).__new__(cls)
         elif conf:
             raise Exception("Dynamic configuration file is not implemented")
 
         return cls._instance
 
-    def __init__(self, conf=None):
+    def __init__(self, conf=None, default=None):
         if not hasattr(self, "initialized"):
-            if not conf:
-                raise Exception("Cannot instance without a configuration file")
-
-            with open(conf, "r") as f:
-                self.config = json.load(f)
+            if conf:
+                if os.path.isfile(conf):
+                    with open(conf, "r") as f:
+                        self.config = json.load(f)
+                else:
+                    self.config = default
+            else:
+                self.config = default
 
             self.initialized = True
